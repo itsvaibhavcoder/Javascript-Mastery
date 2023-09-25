4f96ac0e42ba98f27d459ead23f602183c7aeffc
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2023-09-26 00:46:52 +0530

    Update and rename 3_Operators in Javascript to 3_Operators in Javascript.js

---
+++ 3_Operators in Javascript.js
@@ -0,0 +1,58 @@
+// Types of JavaScript Operators
+// There are different types of JavaScript operators:
+
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// String Operators
+// Logical Operators
+// Bitwise Operators
+// Ternary Operators
+// Type Operators
+
+//Assignment Operators :
+let x = 10;
+// Assign the value 5 to x
+let x = 5;
+// Assign the value 2 to y
+let y = 2;
+// Assign the value x + y to z:
+let z = x + y;
+
+//JavaScript Addition :
+//The Addition Operator (+) adds numbers:
+
+//Adding
+let x = 5;
+let y = 2;
+let z = x + y;
+
+//JavaScript Multiplication
+//The Multiplication Operator (*) multiplies numbers:
+
+//Multiplying
+let x = 5;
+let y = 2;
+let z = x * y;
+
+
+
+/* 
+Math Operators
+Multiply *
+Divide /
+Exponents **
+Modulo/Remainder %  5%2 = 1
+Add +
+Subtract -
+
+*/
+
+/* Math Methods
+Floor - Rounds down
+Ceil - Rounds up
+Random - gives you number between 0 and 1
+*/
+
+
+
