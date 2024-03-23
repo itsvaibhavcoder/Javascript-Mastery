86d13ae0688609d21c3e87f24c9c0c59a4c40d7a
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2024-03-23 23:49:38 +0530

    Add files via upload

---
+++ Javascripts Basics/2_Data types in Javascript.js
@@ -0,0 +1,33 @@
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+
+// BigInt
+let x = BigInt("123456789012345678901234567890");
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+//Note : When adding a number and a string, JavaScript will treat the number as a string.
+let x = 16 + "Volvo";  -----> 16 = "16"
+
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object: (key , value)
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+Note : JavaScript Types are Dynamic 
+let x;       // Now x is undefined
+x = 5;       // Now x is a Number
+x = "John";  // Now x is a String
